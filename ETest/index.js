a1c5ed51ef87d3f3c5e4265b17e0028de5c1228c
Dan Habot <dan.habot@gmail.com>
2019-07-30 16:54:29 -0400

    Did timer

--- ETest/index.js
+++ ETest/index.js
@@ -2,28 +2,36 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 var bodyParser = require('body-parser')
 var api = require('express')()
 var win
-var score, roundTicks, gameTicks
+var score = 0, roundTicksLimit = roundTicks = 6, gameTicksLimit = gameTicks = 6 * 60, roundTicker, gameTicker
 
 function updateUI(channel, msg, res) {
   if (win) {
     win.webContents.send(channel, msg);
-    res.status(200).send('Display updated')
+    res && res.status(200).send('Display updated')
     return true;
   }
   else {
-    res.status(500).send('Error communicating with render process')
+    res && res.status(500).send('Error communicating with render process')
     console.error('No display')
     return false;
   }
 }
 
 api.use(bodyParser.urlencoded({ extended: false }))
+
 api.use((req, res, next) => {
   req.body = Object.keys(req.body)[0]
   next()
 })
-// api.use(bodyParser.json())
-// api.use(bodyParser.raw())
+
+api.use((req, res, next) => {
+  if (gameTicks < 1) {
+    res.status(401).send('Game over')
+  }
+  else {
+    next()
+  }
+})
 
 api.get('/', (req, res) => {
   if (win) {
@@ -48,10 +56,36 @@ api.post('/answer/:label', (req, res) => {
 })
 
 api.post('/start', (req, res) => {
+  roundTicker = setInterval(() => {
+    updateUI('roundtick', roundTicks)
+    if (roundTicks-- < 1) {
+      updateUI('roundsup', '')
+      roundTicks = roundTicksLimit
+      clearInterval(roundTicker)
+    }
+  }, 1000)
 
+  if (!gameTicker) {
+    gameTicker = setInterval(() => {
+      updateUI('gametick', gameTicks)
+      if (gameTicks-- < 1) {
+        updateUI('gameover', '')
+      }
+    }, 1000)
+  }
+
+  res.status(200)
+
+  if (gameTicks == gameTicksLimit) {
+    res.send('Game started')
+  }
+  else {
+    res.send('Round started')
+  }
 })
 
 api.post('/answer/:label/correct', (req, res) => {
+  clearInterval(roundTicker)
   updateUI(req.param.label + 'correct', 'CORRECT', res)
 })
 
